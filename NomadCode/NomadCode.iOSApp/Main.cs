f1bd1c6db64c6832b7e6b44e1751fa11f4792a57
Colby Williams <colbylwilliams@gmail.com>
2016-09-27 11:36:01 -0400

    create xamarin sln and iOS projects

---
+++ NomadCode/NomadCode.iOSApp/Main.cs
@@ -0,0 +1,9 @@
+﻿using UIKit;
+
+namespace NomadCode.iOSApp
+{
+	public class Application
+	{
+		static void Main (string [] args) => UIApplication.Main (args, null, "AppDelegate");
+	}
+}
---
+++ NomadCode/NomadCode.iOSApp/ViewController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using UIKit;
+
+namespace NomadCode.iOSApp
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController (IntPtr handle) : base (handle) { }
+	}
+}
